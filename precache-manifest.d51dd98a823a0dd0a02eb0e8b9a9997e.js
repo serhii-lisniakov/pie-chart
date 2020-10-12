21340045fcafa291475bbb241fe176566574b3b3
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-10-12 13:58:56 +0300

    Updates

---
+++ precache-manifest.d51dd98a823a0dd0a02eb0e8b9a9997e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e71cf60b9792500c391ebe7d606ef60",
+    "url": "/pie-chart/index.html"
+  },
+  {
+    "revision": "c2a3f89d2894758b2184",
+    "url": "/pie-chart/static/css/main.5edfed18.chunk.css"
+  },
+  {
+    "revision": "5bd866445da6f28a2426",
+    "url": "/pie-chart/static/js/2.a485d702.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pie-chart/static/js/2.a485d702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a3f89d2894758b2184",
+    "url": "/pie-chart/static/js/main.cbd349be.chunk.js"
+  },
+  {
+    "revision": "f2d3981a59c99c6d4eca",
+    "url": "/pie-chart/static/js/runtime-main.e1c568cf.js"
+  }
+]);
